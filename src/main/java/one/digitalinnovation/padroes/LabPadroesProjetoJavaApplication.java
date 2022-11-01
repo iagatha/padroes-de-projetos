45519580229604dc921b1e78ba179d9e6dbe395a
iagatha <iagathasouzaa@gmail.com>
2022-11-01 17:22:23 -0300

    api sobre padrão de projeto

---
+++ src/main/java/one/digitalinnovation/padroes/LabPadroesProjetoJavaApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.padroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPadroesProjetoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetoJavaApplication.class, args);
+	}
+
+}
